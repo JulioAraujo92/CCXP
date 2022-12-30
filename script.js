8ecb841f7b54643ad1ed13a3c7177b392efd4e35
Julio Cesar <118136237+JulioAraujo92@users.noreply.github.com>
2022-12-30 16:13:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const dia = document.getElementById('dia')
+const hora = document.getElementById('hora')
+const minuto = document.getElementById('minuto')
+const segundo = document.getElementById('segundo')
+
+const lancamento = "30 jan 2023"
+
+function countDown() {
+    const dataLanc = new Date(lancamento)
+    const hoje = new Date()
+
+    const segTotal = (dataLanc - hoje)/1000;
+
+    const finalDias = Math.floor (segTotal / 60 / 60 / 24);
+    const finalHoras = Math.floor (segTotal / 60 / 60) % 24;
+    const finalMinutors = Math.floor (segTotal / 60) % 60;
+    const finalSegundos = Math.floor (segTotal) % 60;
+
+    dia.innerHTML = finalDias + 'D'
+    hora.innerHTML = formatoTempo(finalHoras) + 'H'
+    minuto.innerHTML = formatoTempo(finalMinutors) + 'M'
+    segundo.innerHTML = formatoTempo(finalSegundos) + 'S'
+}
+function formatoTempo (tempo){
+    return tempo < 10? `0${tempo}` : tempo;
+}
+countDown();
+setInterval (countDown, 1000)
